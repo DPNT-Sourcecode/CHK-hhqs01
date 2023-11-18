0e713f7038f79a1bc83c55e24954feb55e059046
sbx_user1051 <sbx_user1051@169.254.56.77>
2023-11-18 18:19:30 +0000

    Sat Nov 18 18:19:30 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, World!"
+    if not isinstance(friend_name, str):
+        raise TypeError("Input is not a string!")
+    return "Hello, {}}!".format(friend_name)
+
 
