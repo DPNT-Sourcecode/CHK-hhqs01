249c56ae3a1dc732b6b26f5abf5316108c65a216
sbx_user1051 <sbx_user1051@169.254.56.77>
2023-11-18 18:12:36 +0000

    Sat Nov 18 18:12:36 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    # Need to check if x and y in [0, 100]
     return x + y
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,27 +7,38 @@ class TestSum():
 
     def test_sum_out_of_bounds_x(self):
         try:
-            sum_solution.compute(-1, 2)
-        except ValueError as err:
-            assert str(err) == "Input is not in the expected range"
-
-        try:
             sum_solution.compute(101, 2)
         except ValueError as err:
             assert str(err) == "Input is not in the expected range"
 
     def test_sum_out_of_bounds_x_negative(self):
-        pass
+        try:
+            sum_solution.compute(-1, 2)
+        except ValueError as err:
+            assert str(err) == "Input is not in the expected range"
 
     def test_sum_out_of_bounds_x_not_int():
-        pass
+        try:
+            sum_solution.compute("SomeString", 2)
+        except TypeError as err:
+            assert str(err) == "Input is not an integer"
 
     def test_sum_out_of_bounds_y(self):
-        pass
+        try:
+            sum_solution.compute(1, 200)
+        except ValueError as err:
+            assert str(err) == "Input is not in the expected range"
 
     def test_sum_out_of_bounds_y_negative(self):
-        pass
+        try:
+            sum_solution.compute(1, -2)
+        except ValueError as err:
+            assert str(err) == "Input is not in the expected range"
 
     def test_sum_out_of_bounds_y_not_int(self):
-        pass
+        try:
+            sum_solution.compute(1, "String")
+        except TypeError as err:
+            assert str(err) == "Input is not an integer"
+
 
