f5d3ce4271fb120971d476cf161b06e6d2a830d9
sbx_user1051 <sbx_user1051@169.254.56.77>
2023-11-18 19:00:20 +0000

    Sat Nov 18 19:00:20 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,3 +11,17 @@ class TestCheckout():
             checkout_solution.create_basket(skus)
         except ValueError:
             assert True
+
+    def test_basket_valid(self):
+        skus = "AAAABBCCCCCD"
+
+        expected_basket = {
+            "A": 4,
+            "B": 2,
+            "C": 5,
+            "D": 1
+        }
+
+        actual_basket = checkout_solution.create_basket(skus)
+
+        assert actual_basket == expected_basket
