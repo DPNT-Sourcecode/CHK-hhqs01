9c851c1a3e0b7aa82113215a03e4946489a2ef8d
sbx_user1051 <sbx_user1051@169.254.26.173>
2023-11-20 20:58:56 +0000

    Mon Nov 20 20:58:56 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -25,3 +25,16 @@ class TestCheckout():
         actual_basket = checkout_solution.create_basket(skus)
 
         assert actual_basket == expected_basket
+
+    def test_compute_price_for_item_no_offer(self):
+        basket = {
+            "A": 2
+        }
+
+        item = "A"
+
+        actual_price = checkout_solution.compute_price_for_item(item, basket)
+
+        expected_price = 100
+
+        assert expected_price == actual_price
